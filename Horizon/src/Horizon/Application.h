774d49a2ca7be3e52e669817dc2948c3769d7d07
Noah Van Miert <noahvanmiert1@gmail.com>
2023-08-18 22:11:04 +0200

    Added Ref, this is a typedef for std::shared_ptr

--- Horizon/src/Horizon/Application.h
+++ Horizon/src/Horizon/Application.h
@@ -37,7 +37,7 @@ namespace Horizon {
 		bool OnWindowClose(WindowCloseEvent& e);
 
 	private:
-		std::unique_ptr<Window> m_Window;
+		Ref<Window> m_Window;
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
--- Horizon/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Horizon/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -53,7 +53,7 @@ namespace Horizon {
 	}
 
 
-	void OpenGLVertexArray::AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer)
+	void OpenGLVertexArray::AddVertexBuffer(const Ref<VertexBuffer>& vertexBuffer)
 	{
 		HR_CORE_ASSERT(vertexBuffer->GetLayout().GetElements().size(), "VertexBuffer has no layout!");
 
@@ -80,7 +80,7 @@ namespace Horizon {
 	}
 
 
-	void OpenGLVertexArray::SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer)
+	void OpenGLVertexArray::SetIndexBuffer(const Ref<IndexBuffer>& indexBuffer)
 	{
 		glBindVertexArray(m_RendererID);
 		indexBuffer->Bind();
